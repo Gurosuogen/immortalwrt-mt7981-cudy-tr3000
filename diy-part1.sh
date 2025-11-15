d8fa41682498e9c400992c23188111e23733913f
Gurosuogen <36596806+Gurosuogen@users.noreply.github.com>
2025-11-15 16:08:43 +0000

    custom package

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,12 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
-git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+# git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+# git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
+# git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+
+src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main
